7e4bd4b4ab35927868dfd2378efed13cd3f0df2f
Killian Piccerelle <KillianPiccerelle@sfr.fr>
2022-05-01 15:00:49 +0200

    Projet fini

---
+++ Exemple.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector("#mode");
+let span = document.querySelector("span");
+
+if (localStorage.getItem("theme")) {
+    if (localStorage.getItem("theme") == "sombre") {
+        modeSombre();
+    }
+}
+
+btn.addEventListener("click", () => {
+    if (document.body.classList.contains("dark")) {
+        // Mode clair
+        document.body.className = "";
+        span.textContent = "Thème sombre";
+        localStorage.setItem("theme", "clair");
+    } else {
+        // Mode sombre
+        modeSombre();
+    }
+});
+
+function modeSombre() {
+    document.body.className = "dark";
+    span.textContent = "Thème clair";
+    localStorage.setItem("theme", "sombre");
+}
